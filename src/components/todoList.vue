945fe803690ed93e4e6d70d229cd4647cec05ba8
kirahaa <khy4018@gmail.com>
2021-11-28 13:17:45 +0900

    Modify component name

---
+++ src/components/todoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    todolist
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todoList',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
